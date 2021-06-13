744d50186d68c62e178bbbf8d0600a628dc03e61
chetan gupta <59758751+chetanguptamrt@users.noreply.github.com>
2021-06-13 12:35:08 +0530

    Add files via upload

---
+++ eBook Management System/src/java/com/database/DBConnect.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author chetan
+ */
+public class DBConnect {
+    
+    private static Connection conn;
+    
+    public static Connection getConnection(){
+        try{
+            if(conn==null){
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookmanagementsystem", "root", "Champ123@");
+            }
+        } catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+    
+}
